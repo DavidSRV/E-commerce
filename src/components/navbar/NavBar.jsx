31f331c80ad6732c8430b706a521c17a61727795
David Ruiz <david.ruizvargas@hotmail.com>
2022-09-28 02:11:30 -0500

    Se termina nav

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,45 +1,35 @@
 import Cart from "../cart/Cart";
 import { Link } from "react-router-dom";
-import logo from '../../assets/images/logo.svg'
-import cart from '../../assets/images/icon-cart.svg'
+import logo from "../../assets/images/logo.svg";
+import cart from "../../assets/images/icon-cart.svg";
 import { avatar } from "../../utils/utils";
 import "./_NavBar.scss";
 
 export default function NavBar() {
   return (
-    <header>
-      <nav className="container">
+    <header className="header-logo-links">
+      <nav className="container-nav">
         <div className="logo">
-          <Link to="/"><img src={logo} alt="Logo-Sneakers" /></Link> 
+          <Link to="/">
+            <img src={logo} alt="Logo-Sneakers" />
+          </Link>
         </div>
-        <div className="anchors">
-          <a href="#">
-            <Link to="/Collection">Collection</Link>
-          </a>
-          <a href="#">
-            <Link to="/Men">Men</Link>
-          </a>
-          <a href="#">
-            <Link to="/Women">Women</Link>
-          </a>
-          <a href="#">
-            <Link to="/About">About</Link>
-          </a>
-          <a href="#">
-            <Link to="/Contact">Contact</Link>
-          </a>
-        </div>
-        <div className="Cart">
-          <Cart>
-            <Link to="/">
-              <img src={cart} alt="" />
-            </Link>
-          </Cart>
-        </div>
-        <div className="imgUser">
-          <img src={avatar} alt="Img-User" />
+        <div className="links">
+          <Link to="/Collection">Collection</Link>
+          <Link to="/Men">Men</Link>
+          <Link to="/Women">Women</Link>
+          <Link to="/About">About</Link>
+          <Link to="/Contact">Contact</Link>
         </div>
       </nav>
+      <div className="container_cart_avatar">
+        <Cart>
+          <Link to="/">
+            <img className="cart" src={cart} alt="" />
+          </Link>
+        </Cart>
+        <img className="avatar" src={avatar} alt="Img-User" />
+      </div>
     </header>
   );
 }
