6a35ab0bcc74e01014109fa024587a19f00d5c03
David Ruiz <david.ruizvargas@hotmail.com>
2022-09-30 23:36:05 -0500

    Se termina section1 - Falta componente

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,13 @@
+import Counter from "./Counter";
+import './_CardStyle.scss'
+
 export default function Card() {
   return (
     <div className="container-Card">
       <div className="section1">
-        <p>SNEAKER COMPANY</p>
+        <p className="title">SNEAKER COMPANY</p>
         <h1>Fall Limited Edition Sneakers</h1>
-        <p>
+        <p className="parr">
           These low-profile sneakers are your perfect casual wear companion.
           featuring a durable rubber outer sole, they'll withstand everything
           the weather can offer
@@ -14,7 +17,8 @@ export default function Card() {
         <h3>$125.00</h3>
         <p>50%</p>
       </div>
-      <span>$250.00</span>
+      <div className="oldprice">$250.00</div>
+    <Counter/>
     </div>
     
   );
--- src/components/card/Counter.jsx
+++ src/components/card/Counter.jsx
@@ -1,6 +1,6 @@
-import Card from "./Card";
 
 export default function Counter() {return(
     <div>
+        <h1>COUNTER</h1>
     </div>
 );}
