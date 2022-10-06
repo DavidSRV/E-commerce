f7fc7f1e4f226425371314bc7ec834d36331746c
David Ruiz <david.ruizvargas@hotmail.com>
2022-10-05 23:23:10 -0500

    se termina pagina principal

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,10 +1,11 @@
 import "./_SliderSttyle.scss";
 import { sliderImgProducts } from "../../utils/utils.js";
 import { useState } from "react";
-import imgprueba from "../../assets/images/image-product-1.jpg";
 
 export default function Slider() {
-  const [img, setImg] = useState("GroupImg");
+  const {GroupImg} = sliderImgProducts
+
+  const [img, setImg] = useState(0);
 
   const imgSlider = sliderImgProducts.GroupThumbail.map((thumbail, i) => (
     <img
@@ -12,13 +13,14 @@ export default function Slider() {
       src={thumbail}
       className="imgThumbail"
       alt="img-thumbail"
+      onClick={() => setImg([i])}
     />
   ));
 
   return (
     <div className="container-slide">
       <div className="cotainer-imgPrincipal">
-        <img className="imgPricipal" src={imgprueba} alt="img-Principal" />
+        <img className="imgPricipal" src={GroupImg[img]} alt="img-Principal" />
       </div>
       <div className="container-thumbail">{imgSlider}</div>
     </div>
